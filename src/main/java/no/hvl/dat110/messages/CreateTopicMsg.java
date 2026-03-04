b2425eb008db1644dc92c7480ddd3b173610d59b
anitasnes <591491@stud.hvl.no>
2026-03-04 11:30:45 +0100

    A

--- src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -3,14 +3,22 @@ package no.hvl.dat110.messages;
 public class CreateTopicMsg extends Message {
 	
 	// message sent from client to create topic on the broker
+    private String topic;
 
     public CreateTopicMsg(String user, String topic) {
-
+        super(MessageType.CREATETOPIC, user);
+        this.topic = topic;
     }
 
-	// TODO: 
-	// Implement object variables - a topic is required
-	
-	// Complete the constructor, get/set-methods, and toString method
-    // as described in the project text	
+    public String getTopic(){
+        return this.topic;
+    }
+
+    public void setTopic(String topic){
+        this.topic = topic;
+    }
+
+    public String toString(){
+        return "CreateTopicMsg [topic=" + topic + "] " + super.toString();
+    }
 }
--- src/main/java/no/hvl/dat110/messages/PublishMsg.java
+++ src/main/java/no/hvl/dat110/messages/PublishMsg.java
@@ -5,19 +5,31 @@ import no.hvl.dat110.common.TODO;
 public class PublishMsg extends Message {
 	
 	// message sent from client to create publish a message on a topic 
+	private String topic;
+	private String message;
 
 	public PublishMsg(String user, String topic, String message) {
-
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+		this.message = message;
+	}
+	public String getTopic(){
+		return this.topic;
 	}
 
-	// TODO:
-	// Implement object variables - a topic and a message is required
+	public void setTopic(String topic){
+		this.topic = topic;
+	}
 
-	// Complete the constructor, get/set-methods, and toString method
-	// as described in the project text
-	
+	public void setMessage(String message){
+		this.message = message;
+	}
+
 	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return this.message;
+	}
+
+	public String toString(){
+		return "PublishMsg [topic=" + topic + ", message= " + message + "] " + super.toString();
 	}
 }
